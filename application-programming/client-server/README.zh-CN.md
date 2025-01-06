0ff7f91870a49fe24be7554c84813a975c50117a
soetas <3228891558@qq.com>
2025-01-06 11:56:48 +0800

    update files

--- application-programming/client-server/README.zh-CN.md
+++ application-programming/client-server/README.zh-CN.md
@@ -4,6 +4,8 @@
 
 - WWW(World Wide Web): 分布式超文本系统
 - SSL(Secure Socket Layer): 安全套接字层
+- URI(Uniform Resource Identifier)：统一资源标识符
+- MIME(Multipurpose Internet Mail Extension): 多目标因特网邮件扩展
 
 > 客户端/服务器架构
 
@@ -14,4 +16,21 @@
 
 服务器托管
 
+URL格式:
+
+```txt
+prot_sch://user:passwd@host:port/path?query#frag
+
+```
+
+| URL组件  |   描述 |
+| -----   | -----  |
+| prot_sch |  网络协议或下载方案 |
+| user |  用户名或登录 |
+| passwd |  用户密码 |
+| host |  运行web服务器的主机名 |
+| port |  端口号（默认为80） |
+| path |  资源文件或CGI应用的路径 |
+| query | 连接符&分割的一系列键值对 |
+| frag |  指定文档内特定锚 |
 
--- application-programming/client-server/example.py
+++ application-programming/client-server/example.py
@@ -1,4 +1,46 @@
 import unittest
+import http
+import ftplib
+import urllib3
+from urllib.request import urlopen, urlretrieve
+from urllib.parse import urlparse, urlunparse, urljoin, quote, unquote, \
+  quote_plus, unquote_plus, urlencode
 
 class TestWebClientServer(unittest.TestCase):
-  pass
+  def test_urlparse(self):
+    url = 'https://docs.python.org/zh-cn/3.13/library/unittest.html'
+    parse_result = urlparse(url)
+
+    self.assertEqual(parse_result.netloc, 'docs.python.org')
+    self.assertEqual(urlunparse(parse_result), url)
+
+  def test_urljoin(self):
+    base_url = 'http://www.python.org/doc/FAQ.html'
+    new_url = 'http://www.python.org/doc/current/lib/lib.html'
+    self.assertEqual(urljoin(base_url, 'current/lib/lib.html'), new_url)
+
+  def test_urlopen(self):
+    with urlopen('https://httpbin.org/') as res:
+      self.assertEqual(res.geturl(), 'https://httpbin.org/')
+
+  def test_urlretrieve(self):
+    def report_hook(blocks, block_size, total_size):
+      pass
+
+    filename, response_headers = urlretrieve('https://httpbin.org/get', 
+      filename='httpbin-get.json', reporthook=report_hook)
+
+    self.assertEqual(filename, 'httpbin-get.json')
+    self.assertEqual(response_headers.get('Content-Type'), 'application/json')
+
+  def test_quote(self):
+    base = 'http://www/~foo/cgi-bin/s.py?name=joe mama&num=6'
+
+    self.assertEqual(quote(base), 'http%3A//www/~foo/cgi-bin/s.py%3Fname%3Djoe%20mama%26num%3D6')
+    self.assertEqual(quote_plus(base), 'http%3A%2F%2Fwww%2F~foo%2Fcgi-bin%2Fs.py%3Fname%3Djoe+mama%26num%3D6')
+  
+  def test_urlencode(self):
+    search_params = {'name': 'Georgina Garcia', 'email': 'jo@ifo.pr'}
+
+    self.assertEqual(urlencode(search_params), 'name=Georgina+Garcia&email=jo%40ifo.pr')
+
