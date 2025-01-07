191783691c38f87bc1deede131d2d8a0a0524a3b
soetas <3228891558@qq.com>
2025-01-07 16:43:07 +0800

    update files

--- application-programming/regexp/example.py
+++ application-programming/regexp/example.py
@@ -1,5 +1,24 @@
-import re
 import unittest
+from re import match, search, compile, DOTALL
 
 class TestRegexp(unittest.TestCase):
-  pass
+  def test_special_symbol(self):
+    self.assertRegex('bet', 'bat|bet|bit')
+    self.assertRegex('.#\n', compile(r'\...', flags=DOTALL))
+    self.assertRegex('From Submit', '^From')
+    self.assertRegex('/d/bin/ls.sh', r'/bin/ls\.sh$')
+    self.assertRegex('Subject:hi', '^Subject:hi$')
+    self.assertRegex('r3po', '[cr][23][dp][o2]')
+    self.assertRegex(' ', '[^aeiou]')
+    self.assertRegex(' ', '[^\t\n]')
+    self.assertRegex('6299278172612', '[0-9]{5,}')
+    self.assertRegex('</a>', '</?[^>]+>')
+    self.assertRegex('a_', '[A-Za-z]\w*')
+
+  def test_match(self):
+    pass
+
+  def test_search(self):
+    pass
+
+
